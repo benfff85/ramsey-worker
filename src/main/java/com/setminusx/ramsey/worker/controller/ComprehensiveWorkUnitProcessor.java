f54667aa856ae2ace5a1c1fc6431ed1448fe1f74
Ben Ferenchak <ben.ferenchak@gmail.com>
2025-04-25 23:36:22 -0400

    Removed UtilityGraph overhead from ComprehensiveWorkUnitProcessor

--- src/main/java/com/setminusx/ramsey/worker/controller/ComprehensiveWorkUnitProcessor.java
+++ src/main/java/com/setminusx/ramsey/worker/controller/ComprehensiveWorkUnitProcessor.java
@@ -1,11 +1,8 @@
 package com.setminusx.ramsey.worker.controller;
 
 import com.setminusx.ramsey.worker.client.MiddlewareClient;
-import com.setminusx.ramsey.worker.config.RamseyConfig;
 import com.setminusx.ramsey.worker.model.*;
-import com.setminusx.ramsey.worker.utility.UtilityGraph;
 import com.setminusx.ramsey.worker.service.CliqueCheckServiceComprehensiveBitSet;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -15,74 +12,83 @@ import java.util.List;
 
 import static com.setminusx.ramsey.worker.utility.TimeUtility.now;
 
-
+/**
+ * This class is responsible for processing work units in a comprehensive manner.
+ * It uses the BitSet-based logic for clique checking and adjacency matrix construction.
+ */
 @Slf4j
 @Component
 public class ComprehensiveWorkUnitProcessor implements WorkUnitProcessor {
 
     private final CliqueCheckServiceComprehensiveBitSet cliqueCheckService;
-    private final RamseyConfig ramseyConfig;
     private final MiddlewareClient middlewareClient;
-    private UtilityGraph utilityGraph;
 
-
-    public ComprehensiveWorkUnitProcessor(CliqueCheckServiceComprehensiveBitSet cliqueCheckService, RamseyConfig ramseyConfig, MiddlewareClient middlewareClient) {
+    public ComprehensiveWorkUnitProcessor(CliqueCheckServiceComprehensiveBitSet cliqueCheckService, MiddlewareClient middlewareClient) {
         this.cliqueCheckService = cliqueCheckService;
         this.middlewareClient = middlewareClient;
-        this.ramseyConfig = ramseyConfig;
     }
 
-    @PostConstruct
-    public void init() {
-        utilityGraph = new UtilityGraph(ramseyConfig.getVertexCount());
-    }
-
-    private BitSet[] buildAdjacencyMatrix(List<Vertex> vertices, EdgeColor color) {
-        int n = vertices.size();
-        BitSet[] adjacency = new BitSet[n];
-        for (int i = 0; i < n; i++) {
-            adjacency[i] = new BitSet(n);
+    private BitSet[] buildAdjacencyMatrixFromEdgeData(String edgeData, int vertexCount) {
+        BitSet[] adjacency = new BitSet[vertexCount];
+        for (int i = 0; i < vertexCount; i++) {
+            adjacency[i] = new BitSet(vertexCount);
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && vertices.get(i).getEdgeColor(vertices.get(j)).equals(color)) {
+        int edgeIndex = 0;
+        for (int i = 0; i < vertexCount; i++) {
+            for (int j = i + 1; j < vertexCount; j++) {
+                if (edgeData.charAt(edgeIndex) == '1') {
                     adjacency[i].set(j);
+                    adjacency[j].set(i);
                 }
+                edgeIndex++;
             }
         }
         return adjacency;
+    }
+
+    private BitSet[] invertAdjacencyMatrix(BitSet[] adjacency) {
+        int n = adjacency.length;
+        BitSet[] inverted = new BitSet[n];
+        for (int i = 0; i < n; i++) {
+            inverted[i] = new BitSet(n);
+            for (int j = 0; j < n; j++) {
+                if (i != j && !adjacency[i].get(j)) {
+                    inverted[i].set(j);
+                }
+            }
+        }
+        return inverted;
     }
 
     @Override
     public void process(WorkUnit workUnit) {
         log.info("Processing WorkUnit: {}", workUnit);
 
-        if (!workUnit.getBaseGraphId().equals(utilityGraph.getId())) {
-            Graph graph = middlewareClient.getGraphById(workUnit.getBaseGraphId());
-            utilityGraph.applyColoring(graph.getEdgeData(), graph.getGraphId());
+        Graph graph = middlewareClient.getGraphById(workUnit.getBaseGraphId());
+        int vertexCount = graph.getVertexCount();
+        String edgeData = graph.getEdgeData();
+        List<WorkUnitEdge> edgesToFlip = workUnit.getEdgesToFlip();
+
+        // Flip the specified edges in edgeData
+        char[] edgeDataArr = edgeData.toCharArray();
+        for (WorkUnitEdge edge : edgesToFlip) {
+            int i = edge.getVertexOne();
+            int j = edge.getVertexTwo();
+            int edgeIndex = (i * vertexCount - (i * (i + 1)) / 2) + (j - i - 1);
+            edgeDataArr[edgeIndex] = (edgeDataArr[edgeIndex] == '1') ? '0' : '1';
         }
-        workUnit.setProcessingStartedDate(now());
+        String flippedEdgeData = new String(edgeDataArr);
 
-        log.debug("Flipping edges");
-        utilityGraph.flipsEdges(workUnit.getEdgesToFlip());
+        BitSet[] redAdjacency = buildAdjacencyMatrixFromEdgeData(flippedEdgeData, vertexCount);
+        BitSet[] blueAdjacency = invertAdjacencyMatrix(redAdjacency);
 
-        log.debug("Checking for cliques in derived graph");
-        List<Vertex> vertices = utilityGraph.getVertices();
-        BitSet[] redAdjacency = buildAdjacencyMatrix(vertices, EdgeColor.RED);
-        BitSet[] blueAdjacency = buildAdjacencyMatrix(vertices, EdgeColor.BLUE);
-        List<Clique> derivedGraphCliques = new ArrayList<>();
-        derivedGraphCliques.addAll(cliqueCheckService.getCliques(vertices, redAdjacency));
-        derivedGraphCliques.addAll(cliqueCheckService.getCliques(vertices, blueAdjacency));
+        List<List<Integer>> derivedGraphCliques = new ArrayList<>();
+        derivedGraphCliques.addAll(cliqueCheckService.getCliques(vertexCount, redAdjacency));
+        derivedGraphCliques.addAll(cliqueCheckService.getCliques(vertexCount, blueAdjacency));
+
         log.info("Clique count for derived graph: {}", derivedGraphCliques.size());
-
         workUnit.setCliqueCount(derivedGraphCliques.size());
         workUnit.setCompletedDate(now());
         workUnit.setStatus(WorkUnitStatus.COMPLETE);
-
-
-        log.debug("Reverting base graph");
-        utilityGraph.flipsEdges(workUnit.getEdgesToFlip());
-
     }
-
 }
--- src/main/java/com/setminusx/ramsey/worker/service/CliqueCheckServiceComprehensiveBitSet.java
+++ src/main/java/com/setminusx/ramsey/worker/service/CliqueCheckServiceComprehensiveBitSet.java
@@ -2,8 +2,6 @@ package com.setminusx.ramsey.worker.service;
 
 import com.setminusx.ramsey.worker.config.EnablePerfLogging;
 import com.setminusx.ramsey.worker.config.RamseyConfig;
-import com.setminusx.ramsey.worker.model.Clique;
-import com.setminusx.ramsey.worker.model.Vertex;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -19,35 +17,33 @@ public class CliqueCheckServiceComprehensiveBitSet {
     }
 
     @EnablePerfLogging
-    public List<Clique> getCliques(List<Vertex> vertices, BitSet[] adjacency) {
-        List<Clique> cliques = new ArrayList<>();
-        int n = vertices.size();
-        BitSet R = new BitSet(n);
-        BitSet P = new BitSet(n);
-        BitSet X = new BitSet(n);
-        P.set(0, n);
-        bronKerbosch(R, P, X, adjacency, cliques, vertices);
+    public List<List<Integer>> getCliques(int vertexCount, BitSet[] adjacency) {
+        List<List<Integer>> cliques = new ArrayList<>();
+        BitSet R = new BitSet(vertexCount);
+        BitSet P = new BitSet(vertexCount);
+        BitSet X = new BitSet(vertexCount);
+        P.set(0, vertexCount);
+        bronKerbosch(R, P, X, adjacency, cliques, vertexCount);
         return cliques;
     }
 
-    private void bronKerbosch(BitSet R, BitSet P, BitSet X, BitSet[] adjacency, List<Clique> cliques, List<Vertex> vertices) {
+    private void bronKerbosch(BitSet R, BitSet P, BitSet X, BitSet[] adjacency, List<List<Integer>> cliques, int vertexCount) {
         if (R.cardinality() == subgraphSize) {
-            List<Vertex> cliqueVertices = new ArrayList<>();
+            List<Integer> cliqueVertices = new ArrayList<>();
             for (int v = R.nextSetBit(0); v >= 0; v = R.nextSetBit(v + 1)) {
-                cliqueVertices.add(vertices.get(v));
+                cliqueVertices.add(v);
             }
-            cliques.add(new Clique(cliqueVertices));
-            return; // Do not continue expanding this clique
+            cliques.add(cliqueVertices);
+            return;
         }
-        if (R.cardinality() + P.cardinality() < subgraphSize) return; // pruning
-        if (P.isEmpty()) return; // No more candidates to expand
-
+        if (R.cardinality() + P.cardinality() < subgraphSize) return;
+        if (P.isEmpty()) return;
         BitSet candidates = (BitSet) P.clone();
         for (int v = candidates.nextSetBit(0); v >= 0; v = candidates.nextSetBit(v + 1)) {
             R.set(v);
             BitSet newP = (BitSet) P.clone();
             newP.and(adjacency[v]);
-            bronKerbosch(R, newP, X, adjacency, cliques, vertices);
+            bronKerbosch(R, newP, X, adjacency, cliques, vertexCount);
             R.clear(v);
             P.clear(v);
             X.set(v);
